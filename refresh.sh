b56aaf8f3c8bb31e9837c0e3e1e5a11195196380
valentina-anbessa <deckenkang@linkervision.com>
2025-01-16 15:23:06 +0800

    add refresh.sh

---
+++ refresh.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd /Users/decken/Downloads/valentina-anbessa.github.io
+/usr/local/bin/docker exec -i youtube-app-1 python app/refresh_static_site.py > /Users/decken/Downloads/docker.log 2>&1
+mv /Users/decken/Dropbox/Youtube/index.html .
+git add .
+git commit -m "refresh index.html at `date +"%Y-%m-%dT%H:%M:%S%z"`"
+git push origin --force
+
+# yesterday=`date -v '-1d' '+%Y-%m-%d'`
+# commit_id=`git log --after="${yesterday} 00:00" --before="${yesterday} 23:59" --pretty=format:"%h" | head -n 1`
+# git reset --hard ${commit_id}
